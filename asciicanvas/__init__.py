50c6c74cb1bb261fc6394e93d4d794f43366c2ef
Dmitry Alimov <dvalimov@gmail.com>
2016-08-31 12:22:09 +0300

    Update __init__.py

---
+++ asciicanvas/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+ASCII canvas
+
+ASCII canvas module for drawing in console using ASCII chars
+
+ASCII canvas supports the next objects:
+- Point
+- Line
+- Rectangle
+- Nine-Patch Rectangle
+- Ellipse
+- Text
+ 
+And also supports Style for all these objects. The Style includes symbol, 
+foreground color, background color and font style. Thereby colored printing 
+to terminal is supported.
+"""
+
+__title__ = 'asciicanvas'
+__version__ = '0.0.1'
+__author__ = 'Dmitry Alimov'
+__license__ = 'MIT'
